1872bdf7d44f09a7c61b900b36e76a508c038502
mcgaryp <portermcgary@me.com>
2020-03-05 17:06:34 -0700

    update in locations

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
 var app = express();
 
 app.set('port', process.env.PORT || 8000)
-   // .use(express.static('../public'))
-   // .set('views', __dirname + '/views')
-   // .set('view engine', 'ejs')
+   .use(express.static('../public'))
+   .set('views', __dirname + '/views')
+   .set('view engine', 'ejs')
    .get('/', (req, res) => {
       res.sendFile('index.html', { root: '../public' })
    })
