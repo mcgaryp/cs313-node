aa7d924bd44847eea59a040b2c3ff9295b99842f
mcgaryp <portermcgary@me.com>
2020-03-05 17:07:52 -0700

    last update

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 8000)
+   .use(express.static(__dirname + '/public'))
+   .set('views', __dirname + '/views')
+   .set('view engine', 'ejs')
+   .get('/', (req, res) => {
+      res.sendFile('index.html', { root: '../public' })
+   })
+
+   .listen(app.get('port'), function () {
+      console.log('Listening on port: ' + app.get('port'));
+   });
